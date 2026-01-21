b495cb30006a28b0523ffa76fde000ef05270bec
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-01-21 11:58:59 +0100

    Ajout du composant SearchResults pour afficher les résultats de recherche.

---
+++ autocompletion-recettes/src/components/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResults() {
+  return (
+    <div>
+      {/* Résultats de recherche s'afficheront ici */}
+    </div>
+  );
+}
+
+export default SearchResults;
