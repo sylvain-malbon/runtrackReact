3c28255dfd34c910148ea5a6483983aa081878ae
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-01-21 12:01:39 +0100

    Ajout récupération du paramètre 'q' de l’URL dans SearchResults.js avec useSearchParams

--- autocompletion-recettes/src/components/SearchResults.js
+++ autocompletion-recettes/src/components/SearchResults.js
@@ -1,8 +1,13 @@
-import React from 'react';
 
-function SearchResults() {
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('q') || '';
+
   return (
     <div>
+      <h2>Résultats pour : <span style={{color: 'orange'}}>{query}</span></h2>
       {/* Résultats de recherche s'afficheront ici */}
     </div>
   );
