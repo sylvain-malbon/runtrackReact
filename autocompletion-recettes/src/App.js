5e981c07d15f8066092057d7ac1bc95097c920b0
sylvain-malbon <sylvain.malbon@laplateforme.io>
2026-01-21 11:21:22 +0100

    Suppression du doublon SearchBar : composant retiré de App.js, conservé uniquement dans Home.js Ajout de <BrowserRouter> dans index.js pour activer la navigation avec React Router

--- autocompletion-recettes/src/App.js
+++ autocompletion-recettes/src/App.js
@@ -1,14 +1,12 @@
 
 
+
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-import SearchBar from './components/SearchBar';
 
-function App() {
   return (
     <div className="App">
-      <SearchBar />
       <Home />
     </div>
   );
